f2a36fc3709e19226d2df481c13d73b2ad44391f
Natalie Akam <natalie.akam@gmail.com>
2015-10-08 15:34:31 +0100

    Added add posts functionality

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -16,11 +16,17 @@ angular.module('duskyStar.view1', ['ngRoute'])
   self.test = "helwdwlo worlasdd";
 
   self.posts = [
-    'post 1',
-    'post 2',
-    'post 3',
-    'post 4',
-    'post 5'
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 15},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 4}
   ];
+
+  self.addPost = function() {
+    if (!self.title || self.title === '') { return; }
+    self.posts.push({ title: self.title, upvotes: 0 });
+    self.title = '';
+  }
 
 }]);
