f9a157b1f2d61e04396df931a2e0e4d1c0629cd3
Natalie Akam <natalie.akam@gmail.com>
2015-11-02 17:05:45 +0000

    Injected factory into controller

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,17 +12,10 @@ angular.module('duskyStar.view1', ['ngRoute'])
 .controller('View1Ctrl', ['postFactory', function(postFactory) {
 
   var self = this;
-  console.log(postFactory);
 
   self.test = "helwdwlo worlasdd";
 
-  self.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  self.posts = postFactory.posts;
 
   self.addPost = function() {
     if (!self.title || self.title === '') { return; }
